ab25e36f725199c4ac227c117ce741f30f9b6c68
Luke Blondin <lblondin747@gmail.com>
2021-07-28 11:28:39 -0500

    init commit

---
+++ EncapsulationPractice/EncapsulationPractice/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace EncapsulationPractice
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public BankAccount()
+        {
+        }
+        public void Deposit(double d)
+        {
+            balance += d;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ EncapsulationPractice/EncapsulationPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncapsulationPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount user = new BankAccount();
+            bool yes = true;
+            while (yes)
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                user.Deposit(Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine($"New balance: {user.GetBalance()}");
+                Console.WriteLine("Would you like to deposit more?(y/n)");
+                if(Console.ReadLine() != "y")
+                {
+                    yes = false;
+                }
+            }
+        }
+    }
+}
